3bdff20cdcb71e07350f9da89332adbdcb757870
BG <yrtnm1@gmail.com>
2024-04-17 00:54:39 +0900

    임시 인트로, 로비, 게임 씬 추가, 에디터 탭 씬 체인지 매뉴 추가

---
+++ Assets/99,Editor/Scripts/SceneChange.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+
+public class SceneChange : Editor
+{
+    [MenuItem("SceneChange/0.IntroScene")]
+    private static void IntroScene()
+    {
+        EditorSceneManager.OpenScene("Assets/00.Scenes/00.Intro.unity");
+    }
+
+    [MenuItem("SceneChange/1.LobbyScene")]
+    private static void MainScene()
+    {
+        EditorSceneManager.OpenScene("Assets/00.Scenes/01.Lobby.unity");
+    }
+
+    [MenuItem("SceneChange/2.GameScene")]
+    private static void GameScene()
+    {
+        EditorSceneManager.OpenScene("Assets/00.Scenes/01.Game.unity");
+    }
+}
